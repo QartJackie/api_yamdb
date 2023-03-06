4a3690bc53203870ba03e8987318fce92eab5b06
Максим Закиров <gtiw14@gmail.com>
2023-03-06 16:59:20 +0300

    fixes

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,67 +1,12 @@
 from rest_framework import permissions
-"""Нужны першмишены, которые дадут
-оступы по докам. Либо админу, либо
-модератору и админу, либо только чтение.
-По идее хватит 2 пермишшенов,
-но нужно уточнить и обсудить."""
 
 
 class IsAdmin(permissions.BasePermission):
+    """Пермишн для админа"""
     message = 'Недостаточно прав'
 
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
             and request.user.is_admin or request.user.is_superuser
-        )
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    message = 'Недостаточно прав'
-
-    def has_permission(self, request, view):
-        return (
-            (
-                request.user.is_authenticated
-                and (request.user.is_admin or request.user.is_superuser)
-            ) or request.method in permissions.SAFE_METHODS
-        )
-
-
-class IsModerator(permissions.BasePermission):
-    message = 'Недостаточно прав'
-
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.is_moderator or request.user.is_superuser
-        )
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    message = 'Недостаточно прав'
-
-    def has_object_permission(self, request, view, obj):
-        return(
-            request.method in permissions.SAFE_METHODS
-            or obj.user == request.user
-        )
-
-
-class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
-    message = 'Недостаточно прав'
-
-    def has_permission(self, request, view):
-        return (
-            (request.method in permissions.SAFE_METHODS
-             and request.user.is_anonymous)
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.is_admin
-            or request.user.is_moderator
         )
